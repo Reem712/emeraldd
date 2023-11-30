d230d91349930e3083433e7ab557b40b76a3ef1f
reem712 <reemmaher712@gmail.com>
2023-11-30 05:38:37 +0200

    add

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('pageContent').style.display = 'block';
+       document.getElementById('navContent').style.display = 'flex';
+    }, 1000);
+  });
+
+
+  
